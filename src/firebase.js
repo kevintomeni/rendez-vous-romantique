47e62131360f8c7a87656c84abf3074e6725c23d
kevintomeni <tomenikevin@icloud.com>
2026-07-17 21:24:10 +0200

    feat: config Firebase réelle + déploiement initial

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "REMPLACER_PAR_TA_CLE",
-  authDomain: "REMPLACER_PAR_TON_PROJET.firebaseapp.com",
-  projectId: "REMPLACER_PAR_TON_PROJET",
-  storageBucket: "REMPLACER_PAR_TON_PROJET.firebasestorage.app",
-  messagingSenderId: "REMPLACER_PAR_TON_ID",
-  appId: "REMPLACER_PAR_TON_APP_ID"
+  apiKey: "AIzaSyCKmD2yZfZI3usb2_26pSv2diZkFd9Ud4Q",
+  authDomain: "rendez-vous-romantique.firebaseapp.com",
+  projectId: "rendez-vous-romantique",
+  storageBucket: "rendez-vous-romantique.firebasestorage.app",
+  messagingSenderId: "723418369828",
+  appId: "1:723418369828:web:d1ed67eb5b898dc8c42db1"
 }
 
 const app = initializeApp(firebaseConfig)
